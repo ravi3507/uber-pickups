c55a44a4e25763ee2d1b3a45897d4c322d045f1d
Ravi Pandey <78900011+ravi3507@users.noreply.github.com>
2022-09-28 00:27:36 +0530

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,10 +1,13 @@
-import streamlit as st 
-import pandas as pd 
+import streamlit as st
+import pandas as pd
 import numpy as np
-st.title("Pickups")
+
+st.title('Uber pickups in NYC')
+
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
 @st.cache
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
@@ -15,16 +18,19 @@ def load_data(nrows):
 
 data_load_state = st.text('Loading data...')
 data = load_data(10000)
-data_load_state.text('Loading data...done!')
+data_load_state.text("Done! (using st.cache)")
+
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
+
 st.subheader('Number of pickups by hour')
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
-hour_to_filter = 17
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-st.map(filtered_data)
+
+# Some number in the range 0-23
 hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
